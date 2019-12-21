92621ed13a13d7aede47baec605e468024107be9
qualidea01 <yuto2064@gmail.com>
2019-12-21 17:44:51 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <nav
+      id="nav"
+      class="navbar has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="container">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item">Pitmark</router-link>
+          <a
+            role="button"
+            id="navbar-burger"
+            class="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbar-links"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div id="navbar-links" class="navbar-menu">
+          <div class="navbar-end"></div>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <router-view />
+    </main>
   </div>
 </template>
 
+<script>
+export default {
+
+}
+</script>
+
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">サインアップ</h1>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <form class="container">
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="email" placeholder="メールアドレス" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="パスワード" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control">
+            <button class="button is-primary">サインアップ</button>
+          </p>
+        </div>
+      </form>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "sign_up"
+  };
+</script>
+
+<style>
+
+</style>
